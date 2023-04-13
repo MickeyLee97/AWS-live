e01133afe06af975016d2946a35da62cfd1e53ec
MickeyLee97 <130642358+MickeyLee97@users.noreply.github.com>
2023-04-13 15:25:41 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,6 @@
+customhost = "database-2.cguvf4ezgvcl.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "mickeyleeyongwai-employee"
+customregion = "us-east-1"
